621709894fb0e02b9c97d193c938540c29b61d0b
Kratu Bhatt <43086423+kratubhatt@users.noreply.github.com>
2018-09-08 13:21:49 +0530

    Addition Catculator

---
+++ addcal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b,c;
+  printf("\t\t\t\tHello Everyone!!");
+  printf("\n\t\t\tLets Calcuate some Basics!!");
+  printf("\n-------------------------------------------------------------------------------");
+  printf("\n\t\tEnter Two Nums:");
+  scanf("%d %d",&a,&b);
+  c=a+b;
+  printf("\n\t\tAnswer:%d",c);
+  return (0);
+  getch();
+}
